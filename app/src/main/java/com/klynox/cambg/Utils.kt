3398b8f0508cda6e663698007b822cf460f98653
Yobo <yobozorle@yahoo.com>
2020-10-15 08:09:15 +0100

    Add code files

---
+++ app/src/main/java/com/klynox/cambg/Utils.kt
@@ -0,0 +1,22 @@
+package com.klynox.cambg
+
+
+import android.app.ActivityManager
+import android.content.Context
+
+/**
+ * Copyright (c) 2019 by Roman Sisik. All rights reserved.
+ */
+fun isServiceRunning(context: Context, serviceClass: Class<*>): Boolean {
+    try {
+        val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+        for (service in manager!!.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return false
+}
